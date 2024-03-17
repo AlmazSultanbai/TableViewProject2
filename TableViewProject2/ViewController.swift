33da7161120f432872fb4db7f42d836f98b01213
Almaz Sultanbai <aldeco312@gmail.com>
2024-03-17 12:30:45 +0600

    Initial Commit

---
+++ TableViewProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewProject2
+//
+//  Created by Sultanbai Almaz on 17/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
